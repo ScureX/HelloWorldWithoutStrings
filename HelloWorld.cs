1cad8050b6aa976cae1d6f3b70cec64dc0391fe5
ScureX <47725553+ScureX@users.noreply.github.com>
2020-10-31 00:18:35 +0100

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace HelloWorldWithoutStrings
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                    ((char)(Encoding.ASCII.GetBytes(new char[] { ',' })[0] + Encoding.ASCII.GetBytes(new char[] { '<' })[0])).ToString() + "" +
+                    ((char)(Encoding.ASCII.GetBytes(new char[] { ')' })[0] + Encoding.ASCII.GetBytes(new char[] { '<' })[0])).ToString() + "" +
+                    ((char)(Encoding.ASCII.GetBytes(new char[] { '=' })[0] + Encoding.ASCII.GetBytes(new char[] { '/' })[0])).ToString() + "" +
+                    ((char)(Encoding.ASCII.GetBytes(new char[] { '=' })[0] + Encoding.ASCII.GetBytes(new char[] { '/' })[0])).ToString() + "" +
+                    ((char)(Encoding.ASCII.GetBytes(new char[] { '/' })[0] + Encoding.ASCII.GetBytes(new char[] { '@' })[0])).ToString() + "" +
+                    ((char)(Encoding.ASCII.GetBytes(new char[] { '_' })[0] - Encoding.ASCII.GetBytes(new char[] { '?' })[0])).ToString() + "" +
+                    ((char)(Encoding.ASCII.GetBytes(new char[] { ';' })[0] + Encoding.ASCII.GetBytes(new char[] { '<' })[0])).ToString() + "" +
+                    ((char)(Encoding.ASCII.GetBytes(new char[] { '/' })[0] + Encoding.ASCII.GetBytes(new char[] { '@' })[0])).ToString() + "" +
+                    ((char)(Encoding.ASCII.GetBytes(new char[] { ':' })[0] + Encoding.ASCII.GetBytes(new char[] { '8' })[0])).ToString() + "" +
+                    ((char)(Encoding.ASCII.GetBytes(new char[] { '=' })[0] + Encoding.ASCII.GetBytes(new char[] { '/' })[0])).ToString() + "" +
+                    ((char)(Encoding.ASCII.GetBytes(new char[] { ';' })[0] + Encoding.ASCII.GetBytes(new char[] { ')' })[0])).ToString());
+            Console.ReadLine();
+        }
+    }
+}
